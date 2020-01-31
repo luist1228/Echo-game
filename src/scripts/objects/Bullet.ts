41b067ccfcccdf1a1d91268a46a11c73212f6b29
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2020-01-31 14:21:52 -0400

    Implemented Bullet class

--- src/scripts/objects/Bullet.ts
+++ src/scripts/objects/Bullet.ts
@@ -1,4 +1,34 @@
 import { Scene } from 'phaser'
 import {CST} from '../CST'
 
-export default class Bullet extends Phaser.Physics.Arcade.Sprite{}
+export default class Bullet extends Phaser.Physics.Arcade.Sprite{
+
+    speed: number
+    scale: number
+
+    constructor(
+                scene: Phaser.Scene, 
+                x: number, y: number, 
+                speed: number, 
+                sprite: string, 
+                scale: number){
+
+        super(scene, x, y, sprite)
+        
+        this.scale = scale
+        this.setTexture("bullet")
+        this.setScale(this.scale)
+        scene.add.existing(this)
+        scene.physics.world.enableBody(this);
+        this.setX(x)
+        this.setY(y)
+        this.speed = speed
+        this.setDepth(1)
+
+    }
+
+    disappear() {
+        
+    }
+
+}
